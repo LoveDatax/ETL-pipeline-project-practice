0fd220034330c4ac4a83b5914fea1883bc10efdf
Love Adeola <lveadeola@gmail.com>
2025-07-30 02:04:46 +0100

    setup airflow

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from airflow import DAG
+from airflow.standard.providers.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
 def extract(csv_path):
     raw_data = pd.read_csv(csv_path)
     return raw_data
@@ -21,3 +25,39 @@ def etl_pipeline(csv_path, clean_data_file_path):
     processed_data = transform(raw_data)
     load(processed_data, clean_data_file_path)
     return {f"ETL completed, Data saved to {clean_data_file_path}"}
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2025, 7, 30),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+dag = DAG(
+    dag_id="wineetl_pipeline",
+    default_args=default_args,
+    description="A simple ETL pipeline",
+    schedule_interval="@daily", 
+    catchup=False, 
+)
+# ===== Define Tasks =====
+extract_task = PythonOperator(
+    task_id="extract_data",
+    python_callable=extract,
+    dag=dag,
+)
+
+transform_task = PythonOperator(
+    task_id="transform_data",
+    python_callable=transform,
+    dag=dag,
+)
+
+load_task = PythonOperator(
+    task_id="load_data",
+    python_callable=load,
+    dag=dag,
+)
+
+# ===== Set Task Dependencies =====
+extract_task >> transform_task >> load_task
